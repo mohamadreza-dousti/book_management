639f38464dfc0219aa8bf82eb36ca7424d3792e7
mohamadreza <mohamadrezadoustii84@gmial.com>
2025-11-28 16:08:01 +0330

    library.py modified for use LibraryDB class

--- LIBRARY/library.py
+++ LIBRARY/library.py
@@ -1,24 +1,45 @@
+from db import LibraryDB
+
 class Library:
-    books = []
     def __init__(self, title=None, author=None, status=1):
         self.title = title
         self.author = author
         self.status = status
+        db = LibraryDB()
+        db.create_table()
+        db.close()
         
 
     def add_book(self, title, author, status):
-        Library.books.append({'title':title, 'author':author, 'status' :status})
+        #Library.books.append({'title':title, 'author':author, 'status' :status})
+        add = LibraryDB()
+        add.add_book(title, author, status)
+        add.close()
 
-    @classmethod
-    def remove_book(cls, title):
-        cls.books = [item for item in cls.books 
-                         if item['title'] != title]
+
+    def remove_book(self, title):
+        # cls.books = [item for item in cls.books 
+        #                  if item['title'] != title]
+        remove = LibraryDB()
+        remove.remove_book(title)
+        remove.close()
 
     def search_book(self, title):
-        for book in Library.books:
-            if book['status'] != False and book['title'] == title:
-                return book 
+        # for book in Library.books:
+        #     if book['status'] != False and book['title'] == title:
+        #         return book 
+        books = LibraryDB()
+        result = books.show_books()
+        books.close()
+        for book in result:
+            if book[0] == title:
+                if book[2] != False:
+                    return book
 
-    @classmethod
-    def show_books(cls):
-        return cls.books
+
+    def show_books(self):
+        # return cls.books
+        books = LibraryDB()
+        result = books.show_books()
+        books.close()
+        return result
