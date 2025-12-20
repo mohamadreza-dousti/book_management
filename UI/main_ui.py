8b1fe47727d83a85ceac5e0cf5e872e62a4e7586
mohamadreza <mohamadrezadoustii84@gmial.com>
2025-12-20 14:07:10 +0330

    create main_ui to implement changes on window

---
+++ UI/main_ui.py
@@ -0,0 +1,30 @@
+from LIBRARY.library import Library
+
+class MyApp():
+    def __init__(self):
+        pass
+
+    def addBook(self, title, author, available):
+        self.title = title.get()
+        self.author = author.get()
+        self.available = available.get()
+        book = Library()
+        book.add_book(self.title, self.author, self.available)
+
+    def removeBook(self, title):
+        self.title = title.get()
+        del_book = Library()
+        del_book.remove_book(self.title)
+
+    def searchBook(self, title):
+        self.title = title.get()
+        s_book = Library()
+        res = s_book.search_book(self.title)
+        return res
+
+    
+    def showBooks(self):
+        books = Library()
+        result = books.show_books()
+        return result
+
