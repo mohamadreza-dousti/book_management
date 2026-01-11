8013f9c1651a5bec606975f6fee5e2cdc0502934
mohamadreza <mohamadrezadoustii84@gmial.com>
2026-01-11 17:42:49 +0330

    create ejrayi class for responsibility principle

--- UI/ui.py
+++ UI/ui.py
@@ -5,6 +5,8 @@ class Ui(ctk.CTk):
     def __init__(self):
         super().__init__()
 
+        self.ejra = ejrayi()
+
         self.geometry('600x600')
         self.title('Book management')
         self.grid_columnconfigure(0, weight=1)
@@ -43,6 +45,7 @@ class Ui(ctk.CTk):
         self.display = ctk.CTkScrollableFrame(self, width=350, height=250, corner_radius=10)
         self.display.grid(row=7, columnspan=2)
 
+
     def add_book(self):
         self.lable_title = ctk.CTkLabel(self.display, text='Add book')
         self.lable_title.pack(pady=5, fill='both')
@@ -56,7 +59,7 @@ class Ui(ctk.CTk):
         self.avalable = ctk.CTkEntry(self.display, placeholder_text='avalable')
         self.avalable.pack(pady=2)
 
-        self.save_btn_avalable = ctk.CTkButton(self.display, text='save', command=lambda:self.save(self.title, self.author, self.avalable, self.save_btn_avalable))
+        self.save_btn_avalable = ctk.CTkButton(self.display, text='save', command=lambda:self.ejra.save(self.title, self.author, self.avalable, self.save_btn_avalable))
         self.save_btn_avalable.pack(pady=5)
 
     def remove_book(self):
@@ -66,7 +69,7 @@ class Ui(ctk.CTk):
         self.title_rem = ctk.CTkEntry(self.display, placeholder_text='enter title')
         self.title_rem.pack(pady=2)
 
-        self.remove_btn_avalable = ctk.CTkButton(self.display, text='remove', command=lambda:self.remove(self.title_rem, self.remove_btn_avalable))
+        self.remove_btn_avalable = ctk.CTkButton(self.display, text='remove', command=lambda:self.ejra.remove(self.title_rem, self.remove_btn_avalable))
         self.remove_btn_avalable.pack(pady=5)
 
 
@@ -80,10 +83,10 @@ class Ui(ctk.CTk):
         self.option = ['1', '0', 'both']
         self.filter_var = ctk.StringVar(value='status')
 
-        self.filter = ctk.CTkOptionMenu(self.display, values=self.option, variable=self.filter_var, command=lambda value:self.search(self.title_ser, self.scroll_frame,self.filter, value))
+        self.filter = ctk.CTkOptionMenu(self.display, values=self.option, variable=self.filter_var, command=lambda value:self.ejra.search(self.title_ser, self.scroll_frame,self.filter, value))
         self.filter.pack(pady=3)
 
-        self.search_btn_avalable = ctk.CTkButton(self.display, text='search', command=lambda:self.search(self.title_ser, self.scroll_frame, self.filter))
+        self.search_btn_avalable = ctk.CTkButton(self.display, text='search', command=lambda:self.ejra.search(self.title_ser, self.scroll_frame, self.filter))
         self.search_btn_avalable.pack(pady=5)
 
         self.scroll_frame = ctk.CTkScrollableFrame(self.display, width=250, height=30)
@@ -109,14 +112,16 @@ class Ui(ctk.CTk):
         self.lable_title = ctk.CTkLabel(self.display, text='Show books')
         self.lable_title.pack(pady=5)
 
-        self.show_btn_avalable = ctk.CTkButton(self.display, text='show books', command=lambda:self.show(self.show_btn_avalable))
+        self.show_btn_avalable = ctk.CTkButton(self.display, text='show books', command=lambda:self.ejra.show(self.show_btn_avalable, self.scroll_frame))
         self.show_btn_avalable.pack(pady=5)
+
+        self.scroll_frame = ctk.CTkScrollableFrame(self.display, width=250, height=30)
+        self.scroll_frame.pack(pady=15)
 
     def default(self):
         self.lable_err = ctk.CTkLabel(self.display, text='choose your option from 1-4', fg_color='red')
         self.lable_err.pack(pady=20)
-
-
+    
     def get(self):
         self.bck_btn.configure(state='abled', hover=True)
         self.res = self.get_order.get()
@@ -127,6 +132,19 @@ class Ui(ctk.CTk):
                 '4': self.show_books
             }
         switch.get(self.res, self.default)()
+
+    def back(self):
+        self.get_order.delete(0, 'end')
+        self.bck_btn.configure(state='disabled')
+        for self.widget in self.display.winfo_children():
+            self.widget.destroy()
+
+
+
+
+class ejrayi():
+    def __init__(self):
+        pass
 
     def save(self, title, author, avalable, save_btn_avalable):
         save_btn_avalable.configure(state='disabled', fg_color='green',
@@ -163,18 +181,10 @@ class Ui(ctk.CTk):
                 i+=1
 
 
-    def show(self, show_btn_avalable):
+    def show(self, show_btn_avalable, scroll_frame):
         show_btn_avalable.configure(state='disabled')
-        self.scroll_frame = ctk.CTkScrollableFrame(self.display, width=250, height=30)
-        self.scroll_frame.pack(pady=15)
         self.books = MyApp()
         self.result = self.books.showBooks()
         for self.book in self.result:
-            self.book_label = ctk.CTkLabel(self.scroll_frame, fg_color='black', text=f'title:{self.book[0]}  author:{self.book[1]}  status:{self.book[2]}')
+            self.book_label = ctk.CTkLabel(scroll_frame, fg_color='black', text=f'title:{self.book[0]}  author:{self.book[1]}  status:{self.book[2]}')
             self.book_label.pack(pady=5)
-
-    def back(self):
-        self.get_order.delete(0, 'end')
-        self.bck_btn.configure(state='disabled')
-        for self.widget in self.display.winfo_children():
-            self.widget.destroy()
